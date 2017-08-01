efbee268cd6d1aa8e8a148aa2657ab62746ea816
fsasvari <frane.sasvari@gmail.com>
2017-08-01 08:56:56 +0200

    Edit configuration file

--- src/config/uploadify.php
+++ src/config/uploadify.php
@@ -9,7 +9,8 @@ return [
     |
     | Here you may specify the default filesystem disk that should be used
     | by the Uploadify package. If default filesystem disk is not provided,
-    | package will use default filesystem from Laravel configuration.
+    | or is null, package will use default filesystem from Laravel
+    | configuration.
     |
     */
 
@@ -17,11 +18,12 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Default Thumbnail Folder
+    | Default Thumbnail Folder Suffix
     |--------------------------------------------------------------------------
     |
     | Default suffix for path to thumbnail folder. Example: if "path" is set to
-    | "upload/images/logo/", "path_thumb" will be "upload/images/logo/thumb/".
+    | "upload/images/logo/" and "path_thumb_suffix" is set to "thumb/",
+    | "path_thumb" will be "upload/images/logo/thumb/".
     |
     */
 
@@ -29,19 +31,27 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Name Mask
+    | Thumbnail Name Mask
     |--------------------------------------------------------------------------
-    */
-
-    'name_mask' => '{name}-w{width}-h{height}',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Name Separator
-    |--------------------------------------------------------------------------
+    |
+    | Thumbnail name mask for images is consisted of "name", "width" and
+    | "height" parameters. This mask will be used for generating url and for
+    | creating route to image thumbnail.
     |
     */
 
-    'name_separator' => '-',
+    'name_thumb_mask' => '{name}-w{width}-h{height}',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Slug Separator
+    |--------------------------------------------------------------------------
+    |
+    | Separator that will be used when generating slug from image name after
+    | upload. Example: "Avatar IMAGE 22.jpg" => "avatar-image-22.jpg".
+    |
+    */
+
+    'slug_separator' => '-',
 
 ];
