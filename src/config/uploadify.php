c9fe47d9affdfb3ba3a60ae38d1ac15c0bcb02d7
fsasvari <frane.sasvari@gmail.com>
2017-08-03 15:23:46 +0200

    Edit configuration file

--- src/config/uploadify.php
+++ src/config/uploadify.php
@@ -18,12 +18,10 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Cache thumbnail images
+    | Cache processed images
     |--------------------------------------------------------------------------
     |
-    | Set to "true" if you want to cache processed images.
-    |
-    | Values: true, false
+    | Set "true" if you want to cache processed images.
     |
     */
 
@@ -31,26 +29,26 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Default Thumbnail Folder Suffix
+    | Path to processed images folder
     |--------------------------------------------------------------------------
     |
-    | Default suffix for path to thumbnail folder. Example: if "path" is set to
-    | "upload/images/logo" and "path_thumb_suffix" is set to "thumb",
-    | "path_thumb" will be "upload/images/logo/thumb".
+    | Default path to processed images folder. Example: if model path is set
+    | to "images/logo" and "path" is set to "processed", processed images folder
+    | will be "images/logo/processed".
     |
     */
-    'path_thumb_suffix' => 'thumb',
+    'path' => 'thumb',
 
     /*
     |--------------------------------------------------------------------------
-    | Slug Separator
+    | Separator for generating slug from file name
     |--------------------------------------------------------------------------
     |
-    | Separator that will be used when generating slug from image name after
+    | Separator that will be used when generating slug from file name after
     | upload. Example: "Avatar IMAGE 22.jpg" => "avatar-image-22.jpg".
     |
     */
 
-    'slug_separator' => '-',
+    'separator' => '-',
 
 ];
