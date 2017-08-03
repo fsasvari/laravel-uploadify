be5e3092c29bbdd51886a99376075a24f059932c
fsasvari <frane.sasvari@gmail.com>
2017-08-03 09:12:42 +0200

    Update config file

--- src/config/uploadify.php
+++ src/config/uploadify.php
@@ -22,12 +22,12 @@ return [
     |--------------------------------------------------------------------------
     |
     | Default suffix for path to thumbnail folder. Example: if "path" is set to
-    | "upload/images/logo/" and "path_thumb_suffix" is set to "thumb/",
-    | "path_thumb" will be "upload/images/logo/thumb/".
+    | "images/logo/" and "path_thumb_suffix" is set to "thumb/", "path_thumb"
+    | will be "images/logo/thumb/".
     |
     */
 
-    'path_thumb_suffix' => 'thumb/',
+    'path_thumb_suffix' => 'thumb',
 
     /*
     |--------------------------------------------------------------------------
