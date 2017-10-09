f4a48d485d2154b1c9ee7c3f6d8b852ceee6e97f
fsasvari <frane.sasvari@gmail.com>
2017-10-09 19:54:19 +0200

    Create default uploadify routes file

---
+++ src/routes/uploadify.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Uploadify Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('{path}/{options}/{name}.{extension}', '\Uploadify\Http\Controllers\ImageController@show')
+    ->where('path', '[a-z-/]+')
+    ->where('options', '[a-z0-9-_,]+')
+    ->where('name', '.+?')
+    ->where('extension', 'jpe?g|gif|png|JPE?G|GIF|PNG');
